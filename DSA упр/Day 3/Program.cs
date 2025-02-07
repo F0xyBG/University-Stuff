c6b9f974a659eabefc9f620a49af5ae9dfedcbc8
F0xyBG <minsko@abv.bg>
2025-02-07 14:15:31 +0200

    Algorithms and Data Structures exercises

---
+++ DSA упр/Day 3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<int> tree = new Tree<int>(20,
+                                new Tree<int>(22),
+                                new Tree<int>(8,
+                                    new Tree<int>(4),
+                                    new Tree<int>(12,
+                                        new Tree<int>(10),
+                                        new Tree<int>(14))));
+
+        tree.DFS();
+    }
+}
